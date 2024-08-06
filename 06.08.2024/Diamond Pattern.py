beb418027086d03c9e971ea68bdafd819c2eac90
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2024-08-06 20:35:42 +0530

    Create Diamond Pattern.py

---
+++ 06.08.2024/Diamond Pattern.py
@@ -0,0 +1,12 @@
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()
